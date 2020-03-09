47882786453f405c31a5e13de454d2f773131079
미애정 <miaejeong@miaejeong-ui-MacBookPro.local>
2020-03-09 13:19:45 +0900

    200309

--- study/03-웹디자인기능사/200226-just쇼핑몰/script/comm.js
+++ study/03-웹디자인기능사/200226-just쇼핑몰/script/comm.js
@@ -1,9 +1,75 @@
 $(function(){
+    //nav
+    let $nav = $('nav'); //nav 변수 선언
 
-    // 공지사항을 클릭했을 때
-    $('.tab-bbs .cont').on('click', function(){
-        $('.modal-wrap').addClass('on');
+    // nav 태그에 포커스인, 마우스 오버 됐을 때
+    $nav.on('focusin mouseenter', function(){
+        // nav 태그에 .active 추가
+        $(this).addClass('active');
+    });
+    //nav 태그에 포커스아웃, 마우스가 떠났을 때
+    $nav.on('focusout mouseleave', function(){
+        // nav 태그에 .active 삭제
+        $(this).removeClass('active');
     })
+
+    //slide
+    let now = 1; // 현재 보여지는 이미지를 now 변수에 지정
+    let max = 3; // 이미지의 개수를 max 변수에 지정
+    let width = 100; // 이미지 가로사이즈 width 변수에 지정
+
+    setInterval(function(){
+        $('.slide ul').animate({
+            // left 값을 0, -100%, -200%으로 값 변경
+            left : (now*width*(-1))+'%'
+        })
+        // now++;
+
+    },2000)
+
+
+
+    //modal
+    // 공지사항 첫번째 글을 클릭했을 때
+    $('.tab-bbs .cont li:first-child').on('click', function(){
+        // 모달창에 on 클래스를 붙여 보여준다.
+        $('.modal-wrap').addClass('on');
+    });
+
+    // 모달창에 닫기 버튼을 클릭했을 때
+    $('#modal button').on('click', function(){
+        $('.modal-wrap').removeClass('on');
+    });
+
+    //tab
+    // 탭을 클릭했을 때
+    $('.tab-g h2').on('click', function(event){
+        //a링크 무효화
+        event.preventDefault();
+        // 모든 탭의 .on 지운다
+        $('.tab-g').removeClass('on');
+        // 클릭한 탭에 .on 추가한다.
+        $(this).parent().addClass('on');
+    })
+    
+
+    //갤러리 탭을 클릭했을 때
+    // $('.tab-gallery h2').on('click', function(){
+    //     //.tab-gallery에 .on를 추가한다.
+    //     $('.tab-gallery').addClass('on');
+    //     //.tab-bbs에 있는 .on은 삭제한다.
+    //     $('.tab-bbs').removeClass('on');
+    // });
+
+    // //공지사항 탭을 클릭했을 때
+    // $('.tab-bbs h2').on('click', function(){
+    //     //.tab-bbs에 .on를 추가한다.
+    //     $('.tab-bbs').addClass('on');
+    //     //.tab-gallery에 있는 .on은 삭제한다.
+    //     $('.tab-gallery').removeClass('on');
+    // });
+    
+    
 
 
 
