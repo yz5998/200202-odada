f5483906ae23b81e5691ae038491580350cb9bf2
odada <eehd80@naver.com>
2020-03-18 12:11:23 +0900

    남도맛기행 완료

--- study/03-웹디자인기능사/200310-남도맛기행/script/comm.js
+++ study/03-웹디자인기능사/200310-남도맛기행/script/comm.js
@@ -44,4 +44,46 @@ $(function(){
 
     });
 
+    // tab
+    // .tab-wrap을 모두 찾아서...
+    $('.tab-wrap').each(function(){
+        // .tab-g > h3를 클릭했을 때
+        $('.tab-g > h3').on('click', function(e){
+            // link 해제
+            e.preventDefault();
+            // .tab-g에 붙어있는 모든 .on 지워주고
+            $('.tab-g').removeClass('on');
+            // 클릭한 .tab-g > h3의 부모 .tab-g에게 .on 추가
+            $(this).parent().addClass('on');
+        });
+    });
+
+    // slide
+    // 변수 설정
+    let now = 1, // 현재 이미지를 now 변수에 지정
+        max = 3, // 이미지 갯수를 max 변수에 지정
+        height = 100, // 이미지의 높이값을 height 변수에 지정
+        슬라이드 = $('.slide ul');
+
+    setInterval(function(){ // 주기적으로 실행 함수        
+
+        if(now < 3){ // 현재 이미지가 첫번째, 두번째 이미지일 경우 (now = 1, 2)
+            
+            // .slide ul을 애니메이션 시켜
+            슬라이드.animate({
+                top : (now * height * (-1)) + 'vh'
+            });
+            now++; // now 변수에 1씩 더해 //now = now + 1;
+
+        }else{ // 세번째 이미지일 경우
+            슬라이드.animate({
+                top : 0
+            });
+            now = 1; //now 변수에 1을 대입
+        }
+
+
+    },3000) // 3초마다 실행
+
+
 });
